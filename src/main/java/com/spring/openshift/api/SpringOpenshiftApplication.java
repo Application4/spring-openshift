7901f29de8e1b8f9f1cd7e04bdca06cb3c28a857
Basant kumar Hota <basahota@users.noreply.github.com>
2018-06-01 16:24:48 +0530

    Update SpringOpenshiftApplication.java

--- src/main/java/com/spring/openshift/api/SpringOpenshiftApplication.java
+++ src/main/java/com/spring/openshift/api/SpringOpenshiftApplication.java
@@ -12,7 +12,7 @@ public class SpringOpenshiftApplication {
 
 	@GetMapping("/")
 	public String redirect() {
-		return "Welcome Page....";
+		return "@ Welcome Page @";
 	}
 
 	@GetMapping("/{name}")
